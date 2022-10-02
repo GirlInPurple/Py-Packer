2014014445ad91207058c4748f8401ebcce45db4
GirlInPurple <turtlenerd603@gmail.com>
2022-10-01 21:59:54 -0400

    menus now semi functional and more clarification on small things

--- PyPacker.py
+++ PyPacker.py
@@ -23,7 +23,43 @@ class utils: #startup and other universal utils
         global BOLD
         global UNDERLINE
         
-        if ColorToggle != "false":
+        try:
+            if ColorToggle != "false":
+                BLACK = '\u001b[30m'
+                RED = '\u001b[31m'
+                BRED = '\u001b[41;1m'
+                GREEN = '\u001b[32m'
+                BGREEN = '\u001b[42;1m'
+                YELLOW = '\033[92m'
+                BYELLOW = '\u001b[43;1m'
+                BLUE = '\u001b[34m'
+                MAGENTA = '\u001b[35m'
+                BMAGENTA = '\u001b[45;1m'
+                CYAN = '\u001b[36;1m'
+                WHITE = '\u001b[37m'
+                BWHITE = '\u001b[47;1m'
+                RESET = '\u001b[0m'
+                BOLD = '\033[1m'
+                UNDERLINE = '\033[4m'
+            else:
+                BLACK = '\u001b[0m'
+                RED = '\u001b[0m'
+                BRED = '\u001b[0m'
+                GREEN = '\u001b[0m'
+                BGREEN = '\u001b[0m'
+                YELLOW = '\u001b[0m'
+                BYELLOW = '\u001b[0m'
+                BLUE = '\u001b[0m'
+                MAGENTA = '\u001b[0m'
+                BMAGENTA = '\u001b[0m'
+                CYAN = '\u001b[0m'
+                WHITE = '\u001b[0m'
+                BWHITE = '\u001b[0m'
+                RESET = '\u001b[0m'
+                BOLD = '\u001b[0m'
+                UNDERLINE = '\u001b[0m'
+        except:
+            print("Color Handling Failed, errors might occour later")
             BLACK = '\u001b[30m'
             RED = '\u001b[31m'
             BRED = '\u001b[41;1m'
@@ -40,23 +76,7 @@ class utils: #startup and other universal utils
             RESET = '\u001b[0m'
             BOLD = '\033[1m'
             UNDERLINE = '\033[4m'
-        else:
-            BLACK = '\u001b[0m'
-            RED = '\u001b[0m'
-            BRED = '\u001b[0m'
-            GREEN = '\u001b[0m'
-            BGREEN = '\u001b[0m'
-            YELLOW = '\u001b[0m'
-            BYELLOW = '\u001b[0m'
-            BLUE = '\u001b[0m'
-            MAGENTA = '\u001b[0m'
-            BMAGENTA = '\u001b[0m'
-            CYAN = '\u001b[0m'
-            WHITE = '\u001b[0m'
-            BWHITE = '\u001b[0m'
-            RESET = '\u001b[0m'
-            BOLD = '\u001b[0m'
-            UNDERLINE = '\u001b[0m'
+
 
     global mineVersions
     mineVersions = { #Every MC version this is compatable with
@@ -98,61 +118,72 @@ class utils: #startup and other universal utils
 
             if foundVersion == "false":
                 return "failed"
+    def LocalFiles(self):
+        return os.path.dirname(os.path.realpath(__file__))
 
 class main: #the main program
     def __init__(self):
         
         data = []
+        global settings
         settings = []
         
-        with open("settings.txt","r") as f:
-            data = f.readlines() #readlines() returns a list of items in said file
+        try:
+            with open("settings.txt","r") as f:
+                data = f.readlines() #readlines() returns a list of items in said file
+                print(data)
 
-        for i in range(0, 4): #list loop, the console is (unintentionally) spammed for a bit
-            print(data[i])
-            settings.append(data[i])
-            print(settings)
-        
-        #begin settings handler
-        
-        #version
-        global Dversion
-        if utils.VersionHandler(self, settings[0]) == "failed":
-            print("error while setting default version, setting to 1.19.2")
-            Dversion = "1.19.2"
-        else:
-            Dversion = data[0]
-            print("loaded default version:" + Dversion)
-        
-        #filepath
-        global filePath
-        if settings[1] == "local\n":
-            filePath = os.path.dirname(os.path.realpath(__file__))
-            print('filepath set to "local"')
-        else:
-            filePath = settings[1]
-            print("chosen filepath:" + filePath)
-        
-        #colour
-        global ColorToggle
-        if settings[2] == "true":
-            ColorToggle = "false"
-            print("colored text off")
-        else:
-            ColorToggle = "true"
-            print("colored text on")
+            for i in range(0, 5): #list loop, the console is (unintentionally) spammed for a bit
+                print(data[i])
+                settings.append(data[i])
+                print(settings)
+            
+            #begin settings handlers:
+            
+            #version, needs to be completly redone it jsut spitting out constant errors
+            global Dversion
+            if utils.VersionHandler(self, settings[0]) == "failed":
+                print("error while setting default version, setting to 1.19.2")
+                Dversion = "1.19.2"
+            else:
+                Dversion = data[0]
+                print("loaded default version:" + Dversion)
+            
+            #filepath
+            global filePath
+            if settings[1] == "local\n":
+                filePath = utils.LocalFiles("")
+                print('filepath set to "local"')
+            else:
+                filePath = settings[1]
+                print("chosen filepath:" + filePath)
+            
+            #color
+            global ColorToggle
+            if settings[2] == "true":
+                ColorToggle = "false"
+                print("colored text off")
+            else:
+                ColorToggle = "true"
+                print("colored text on")  
+            
+        except:
+            print("error while handling settings, loading default settings, some errors might occour later")
+            settings.append("1.19.2\n")
+            settings.append("local\n")
+            settings.append("true\n")
+            settings.append("1\n")
+            settings.append("false")
+            
 
     def main():
-        
-        print("project in progress, deafulting to Datapack file creator")
-        files.DPfiles() 
-        #set this as the default for now as i build up main.main()
         
         #settings: (also a settings.txt explanation)
         #   default version (set to 1.19.2 by default)
         #   default file location (set to local by default)
         #   color toggle
         #   first login (with a reset button in case a package or this breaks)
+        #   debug mode (useless atm)
         #opens:
         #   DPfiles() for datapacks
         #   RPfiles() for resrouce packs/texture packs
@@ -161,8 +192,50 @@ class main: #the main program
         #   Dp, Rp, and Sc editors are
         #mc utils:
         #   portal cords, 3d space, and circle calculators all in one
-        #   remote server connect via a
-        return
+        
+        print(BGREEN + 'Welcome to PyPacker! Where would you like to start?\n(Case sensitve, all lowercase; "open" and "output" not guaranteed to work on all Lunix distros)' + RESET)
+        #"[name] > [command]"+ RESET +"\n"+ BWHITE + BLACK + paste this between a "black+" and begining of the next command name.
+        lastMain = input(BWHITE + BLACK + "Open Program Folder > open"+ RESET +"\n"+ BWHITE + BLACK + "Open Output Folder > output"+ RESET +"\n"+ BWHITE + BLACK +"Launch Datapack File Generator > dpfiles"+ RESET +"\n"+ BWHITE + BLACK +"Quit PyPacker > quit\n" + RESET + "\n\n" + CYAN)
+        validMain = "false"
+        while validMain == "false":
+        
+            if lastMain == "open":
+                if sys.platform=='win32':
+                    os.system("start " + utils.LocalFiles(""))
+
+                if sys.platform=='darwin':
+                    os.system("open " + utils.LocalFiles(""))
+
+                input(RESET + BYELLOW + "Program File has been opened, press enter to choose another option." + RESET + CYAN)
+                lastMain = ""
+                
+            elif lastMain == "output":
+                if sys.platform=='win32':
+                    os.system("start " + filePath)
+
+                if sys.platform=='darwin':
+                    os.system("open " + filePath)
+
+                input(RESET + BYELLOW + "Output File has been opened, press enter to choose another option." + RESET + CYAN)
+                lastMain = ""
+                
+            elif lastMain == "quit":
+                input(RESET + BYELLOW + "Press enter to quit." + RESET + CYAN)
+                sys.exit()
+                
+            elif lastMain == "settings":
+                print(RESET + BYELLOW + "settings not impelmented yet" + RESET + CYAN)
+                lastMain = ""
+                
+            elif lastMain == "dpfiles":
+                print(RESET + BYELLOW + "launching DP file generator, ALT+F4 at any time to abort" + RESET + CYAN)
+                files.DPfiles()
+                
+            elif lastMain == "rpfiles":
+                return
+            
+            else:
+                lastMain = input(RESET + RED + "Invalid argument. Please send a valid word.\n" + RESET + CYAN)
         
 class files: #file makers
     def __init__():
@@ -170,6 +243,7 @@ class files: #file makers
     
     def DPfiles():
         packName = input(BGREEN + "Welcome to DataCreate! How do you want to name your datapack?" + RESET + "\n\n" + CYAN)
+        time.sleep(0.5)
         dataName = input(RESET + BYELLOW + "How do you want to name your namespace?" + RESET + "\n\n" + CYAN).lower()
         validName = "false"
         while validName == "false":
@@ -177,7 +251,7 @@ class files: #file makers
                 validName = "true"
             else:
                 dataName = input(RESET + RED + "Namespace not valid. Namespaces can not use specials characters. Please give a valid name.\n" + RESET + CYAN)
-
+        time.sleep(0.5)
         dataVer = input(RESET + BMAGENTA + "What is the minecraft version of your pack? (Leave blank for your chosen default version, currently: " + Dversion + ")" + RESET + "\n\n" + CYAN)
 
         if dataVer == "":
@@ -192,8 +266,9 @@ class files: #file makers
 
                 if foundVersion == "false":
                     dataVer = input(RESET + RED + "Version not found. Please give a supported version.\n" + RESET + CYAN)
-
+        time.sleep(0.5)
         dataDesc = input(RESET + BWHITE + "What is the description of your datapack?" + RESET + "\n\n" + CYAN)
+        time.sleep(0.5)
         dataPath = input(RESET + BRED + "Where do you want to create your datapack? (Leave blank for your chosen default filepath, currently: " + filePath + ")" + RESET + "\n\n" + CYAN)
 
         if dataPath == "":
@@ -258,28 +333,29 @@ class files: #file makers
         os.mkdir("noise_settings")
         os.mkdir("processor_list")
         os.mkdir("template_pool")
-        lastR = input(RESET + BGREEN + "Your datapack has been generated! What do you want to do?" + RESET + "\n\n" + BWHITE + BLACK + "Open folder > open\nQuit PyPacker > quit\nEdit Datapack > Edit" + RESET + CYAN + "\n\n")
-        validR = "false"
-        while validR == "false":
+        lastDP = input(RESET + BGREEN + "Your datapack has been generated! What do you want to do?" + RESET + "\n" + BWHITE + BLACK + "Open folder > open\nQuit PyPacker > quit\nEdit Datapack > Edit" + RESET + CYAN + "\n\n")
+        time.sleep(10)
+        validDP = "false"
+        while validDP == "false":
         
-            if lastR == "open":
+            if lastDP == "open":
                 if sys.platform=='win32':
                     os.system("start "+ dataPath)
 
                 if sys.platform=='darwin':
                     os.system("open " + dataPath)
 
-                input(RESET + BYELLOW + "File has been opened, press enter to choose another option. (Made by Origaming)" + RESET)
-                lastR = ""
-            elif lastR == "quit":
-                input(RESET + BYELLOW + "Thanks for using DataCreate! Press enter to quit. (Made by Origaming)" + RESET)
+                input(RESET + BYELLOW + "File has been opened, press enter to choose another option. (Original DPfiles made by Origaming)" + RESET)
+                lastDP = ""
+            elif lastDP == "quit":
+                input(RESET + BYELLOW + "Press enter to quit. (Original DPfiles made by Origaming)" + RESET)
                 sys.exit()
-            elif lastR == "open":
-                input(RESET + BYELLOW + "Thanks for using DataCreate! Press enter to enter editor, Alt+F4 to abort. (Made by Origaming)" + RESET)
+            elif lastDP == "open":
+                input(RESET + BYELLOW + "Press enter to enter editor, Alt+F4 to abort. (Original DPfiles made by Origaming)" + RESET)
                 #os.system("DP_editor")
                 sys.exit()
             else:
-                lastR = input(RESET + RED + "Invalid argument. Please send a valid word.\n" + RESET + CYAN)
+                lastDP = input(RESET + RED + "Invalid argument. Please send a valid word.\n" + RESET + CYAN)
                 
     def RPfiles():
         pass
@@ -287,21 +363,34 @@ class files: #file makers
     def SCfiles():
         pass
 
-class editors: #file editors, note here
+class editors: #file editors, notes here going forward
+    class test:
+        def __init__():
+            #so you can put a class in a class, i was wondering if this could be an issue, thankfully it looks not: https://www.geeksforgeeks.org/inner-class-in-python/
+            pass
     
     #this will come later, not sure how im gonna do this part yet
     
     #pros of pygame: simple to create and maintain long-term
     #cons of pygame: can't maximize window(?), needs downloading
+    #link: https://www.pygame.org/news
     
     #pros of elctron.py: can port the whole program over to a much better system
     #cons of electron.py: i have no clue how to code good HTML/CSS/JS, only the basics i picked up while messing with F12 on random sites
+    #link: https://github.com/fyears/electron-python-example
+    #note: Eel looks like a better option if i end up going for this: https://github.com/python-eel/Eel
     
     #pros of PyGt5: good ui generator with alot of features
     #cons of PyQt5: its gonna be a bit complex to implement a system of this size in it, needs downloading
+    #link: https://riverbankcomputing.com/software/pyqt/intro
     
     #pros of the command line: simple, understandable for most people with even a little computer knowlage
-    #cons of the command line: hard to work with at times (unicode, drawing graphs, more complex visuals), not everyone understnads it
+    #cons of the command line: hard to work with at times (unicode, drawing graphs, more complex visuals), not everyone understands it
+    #link: n/a
+    
+    #pros of python arcade: Very similar to alot of game/gui libraies, including Pygame from the looks of it.
+    #cons of python arcade: Learned about it just a few hours ago, so ill have to look more into it to proporly rate this, but keeping it here for refrence
+    #link: https://api.arcade.academy/en/latest/index.html
     
     def __init__(self):
         pass
--- README.md
+++ README.md
@@ -11,4 +11,16 @@ A work in progress CUI based pack maker for Java 1.19.2 Minecraft
 # Contributors:
 - @OrigamingWasTaken (original creator of DataCreate.py, a part of this projects foundation [(click here)](https://github.com/OrigamingWasTaken/DataCreate))
 
+# Settings.TXT explained
+- default version (set to 1.19.2)
+- default file location (set to the programs file location)
+- color toggle (text colors on by default, can toggle incase it either becomes unsupported or breaks something)
+- first login (also an unintentional log-in counter, used for setting up default at the start)
+- debug mode (false by default)
+
+# Known Issues
+- Straight up refuses to launch on anything else but VScode, and when it does colors are all messed up; good news, works perfect in said VScode!
+- Due to OS limitations, you cant name 2 packs the same thing, a change is planned that will add "_1" at the end of duplicate packs.
+- settings.txt has some issues atm, specifially chossen version refuses to load
+
 Anyone is free to modify, contribute, or change this program in any way you see fit.
